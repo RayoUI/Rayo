6b21e27776fd7dfd455164ae101a35c04ce72fd4
StrongCod3r <strong.cod3r@gmail.com>
2026-07-25 17:42:29 +0200

    update scripts

--- Scripts/Publish-GitHubActions.ps1
+++ Scripts/Publish-GitHubActions.ps1
@@ -1,4 +1,4 @@
-# .\Scripts\Publish-GitHubActions.ps1 -Version 0.1.10
+# .\Scripts\Publish-GitHubActions.ps1 -Version 0.1.11
 
 [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
 param(
